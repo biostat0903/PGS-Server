b36dd4e4cce6edd79bfe02bb02b33a8138beaf4e
Sheng Yang <yangsheng@njmu.edu.cn>
2021-07-07 07:57:39 +0800

    Delete test_scripts directory

--- test_scripts/external_test.sh
+++
@@ -1,7 +0,0 @@
-pop=EUR
-external=/home/yasheng/comprsWeb/scripts/external.sh
-extsumm=/home/yasheng/comprsWeb/example_data/ext/extsumm.ldsc.gz
-esteff=/home/yasheng/comprsWeb/example_data/output/lassosum_esteff.txt,1,2,3
-outpath=/home/yasheng/comprsWeb/example_data/ext/
-
-sh ${external} -s ${esteff} -e ${extsumm} -p ${pop} -o ${outpath}
