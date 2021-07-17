0ec65a9e5f3576ffe4739cb7b5f5eaad6bbba5c0
Sheng Yang <yangsheng@njmu.edu.cn>
2021-07-17 12:46:27 +0800

    Update lassosum.sh

--- script/03_lassosum/lassosum.sh
+++ script/03_lassosum/lassosum.sh
@@ -27,14 +27,21 @@ printf "\033[33mArgument codepath is %s  \033[0m\n" "$codepath"
 printf "\033[33mArgument summary_file_prefix is %s  \033[0m\n" "$summary_file_prefix"
 printf "\033[33mArgument valid_geno is %s  \033[0m\n" "$val_geno"
 printf "\033[33mArgument valid_pheno is %s  \033[0m\n" "$val_pheno"
-if [ !${cov} ]
+printf "\033[33mArgument pop is %s  \033[0m\n" "$pop"
+if [ ${cov} ]
 then 
 printf "\033[33mArgument cov is %s  \033[0m\n" "$cov"
+fi
+if [ ${thread} ]
+then 
+printf "\033[33mArgument thread is %s  \033[0m\n" "$thread"
+else
+thread=1
 fi
 printf "\033[33mArgument outpath is %s  \033[0m\n" "$outpath"
 
 
-LASSOSUM=${codepth}/lassosum.R
+LASSOSUM=${codepath}/lassosum.R
 
 ## sample size of GWAS
 nobs=`sed -n "2p" ${summary_file_prefix}.assoc.txt | awk '{print $5}'`
@@ -44,10 +51,10 @@ n=$(echo "${nobs}+${nmis}" | bc -l)
 if [ !${cov}  ]
 then 
 
-Rscript ${LASSOSUM} --summ ${summary_file_prefix}.assoc.txt --valid_genotype ${val_geno} --valid_phenotype ${val_pheno}.txt\
+Rscript ${LASSOSUM} --summ ${summary_file_prefix}.assoc.txt --valid_genotype ${val_geno} --valid_phenotype ${val_pheno}\
                     --n ${n} --population ${pop} --outpath ${outpath} --thread ${thread}
 else
 
-Rscript ${LASSOSUM} --summ ${summary_file_prefix}.assoc.txt --valid_genotype ${val_geno} --valid_phenotype ${val_pheno}.txt\
+Rscript ${LASSOSUM} --summ ${summary_file_prefix}.assoc.txt --valid_genotype ${val_geno} --valid_phenotype ${val_pheno}\
                     --n ${n} --population ${pop} --covariates ${cov} --outpath ${outpath} --thread ${thread}
 fi
